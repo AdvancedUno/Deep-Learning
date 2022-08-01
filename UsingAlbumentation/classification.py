e9fe45d55e6b4b20bc2f671b8048c1889e39cf0f
AdvancedUno <ehlee8276@gmail.com>
2022-08-01 12:57:46 +0900

    classification data augmentation

--- UsingAlbumentation/classification.py
+++ UsingAlbumentation/classification.py
@@ -1,11 +1,46 @@
-from PIL import Image
+
+import cv2
+from matplotlib import pyplot as plt
 from utils import plot_examples
+import numpy as np
+import albumentations as A
+from PIL import Image
 
-image = Image.open("test.bmp")
-
-transform = A.Compose([
-
-    A.
+image = Image.open("images/test.jpeg")
 
 
-])
+
+transform = A.Compose(
+    [
+        A.Resize(width=1920, height=1080),
+        A.RandomCrop(width=1280, height=720),
+        A.Rotate(limit=40, p = 0.9, border_mode=cv2.BORDER_CONSTANT),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.1),
+        A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=0.9),
+        A.OneOf(
+            [
+                A.Blur(blur_limit=3, p=0.5),
+                A.ColorJitter(p=0.5),
+            ], p =1.0
+        ),
+    ]
+)
+
+images_list = [image]
+image = np.array(image)
+for i in range(15):
+    augmentations = transform(image=image)
+    augmented_img = augmentations["image"]
+    images_list.append(augmented_img)
+
+
+
+
+
+
+
+
+
+
+
